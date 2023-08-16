46c9cad26deb3441e59c2ba9bc6052574857988c
ji won lee <ljw03079@naver.com>
2023-08-16 17:40:14 +0900

    Board Commit

--- H20230809/src/co/yedam/classes/friend/FriendMain.java
+++ H20230809/src/co/yedam/classes/friend/FriendMain.java
@@ -2,7 +2,7 @@ package co.yedam.classes.friend;
 
 public class FriendMain {
 	public static void main(String[] args) {
-		System.out.println(args[0]+args[1]+args[2]+args.length);
+		//System.out.println(args[0]+args[1]+args[2]+args.length);
 		//FriendApp.start(); //정적 메소드, static이 붙는 메소드
 		//static이 없는 인스턴스 메소드는 호출해야 기능을 쓸 수 있음
 		FriendApp app = new FriendApp();
